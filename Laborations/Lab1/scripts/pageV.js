2be6b6d9f9c520097f5eca6ac50df2384f2b894a
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-05-20 18:44:50 +0200

    added page V, also including some javascript to this page

---
+++ Laborations/Lab1/scripts/pageV.js
@@ -0,0 +1,19 @@
+﻿function RangeUpdate() {
+    var value1 = text_id.value;
+    range_id.value = value1;
+}
+function TextUpdate() {
+    var value1 = range_id.value;
+    text_id.value = value1;
+}
+//function ValidateActivity(activity_id)
+//{
+//    if(activity_id.value=="all")
+//    {
+//        alert("You cant have 'all' in the activity dropdown list");
+//    }
+//    else
+//    {
+
+//    }
+//}
